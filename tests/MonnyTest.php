8026dd80ac04e6647101239226ff8f2b0132794e
sazanami5 <hbws001@gmail.com>
2023-01-05 14:29:52 +0900

    fix:変数修正

--- tests/MonnyTest.php
+++ tests/MonnyTest.php
@@ -10,7 +10,9 @@ class MonnyTest extends TestCase
 {
     public function testMultiplication(){
         $five = new Dollar(5);
-        $five->times(2);
-        $this->assertSame($five->amount, 10);
+        $product = $five->times(2);
+        $this->assertSame($product->amount, 10);
+        $product = $five->times(3);
+        $this->assertSame($product->amount,15);
     }
 }
