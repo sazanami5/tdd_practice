1fe9e6025bfb905ccfe700b9a8f15ff3ae074110
sazanami5 <hbws001@gmail.com>
2023-04-24 16:38:43 +0900

    feat:bankのreduceのクラス条件を削除

--- src/Bank.php
+++ src/Bank.php
@@ -6,9 +6,6 @@ class Bank
 {
     public function reduce(Expression $source, string $to): Money
     {
-        if($source instanceof Money) {
-            return $source->reduce($to);
-        }
         return $source->reduce($to);
     }
 }
