05e29f20f48366ddb21bdbeba8e0dc3077b7ba71
sazanami5 <hbws001@gmail.com>
2023-05-23 14:12:37 +0900

    feat: Sumのtimesメソッドを追加し、Expressionにtimes追加

--- src/Sum.php
+++ src/Sum.php
@@ -21,4 +21,9 @@ class Sum implements Expression
     {
         return new Sum($this, $addend);
     }
+
+    public function times(int $multiplier): Expression
+    {
+        return new Sum($this->augend->times($multiplier), $this->addend->times($multiplier));
+    }
 }
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -86,6 +86,17 @@ class MoneyTest extends TestCase
         $result = $bank->reduce($sum, 'USD');
         $this->assertObjectEquals(Money::dollar(15), $result);
     }
+
+    public function testSumTimesMoney()
+    {
+        $fiveBucks = Money::dollar(5);
+        $tenFrancs = Money::franc(10);
+        $bank = new Bank();
+        $bank->addRate('CHF', 'USD', 2);
+        $sum = (new Sum($fiveBucks, $tenFrancs))->times(2);
+        $result = $bank->reduce($sum, 'USD');
+        $this->assertObjectEquals(Money::dollar(20), $result);
+    }
 }
 
 
